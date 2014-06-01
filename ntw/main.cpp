4e529c33be387f511bc136b418462e16088f2a75
Gideon Williams <gaw5951@gmail.com>
2014-06-01 19:44:12 -0400

    Initial Commit

---
+++ ntw/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ntw
+//
+//  Created by Gideon on 6/1/14.
+//  Copyright (c) 2014 Gideon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
